bbbdb0100f80105106c63a4020e294be651cd065
Лукьянов Дмитрий <diman2421@mail.ru>
2024-02-04 15:24:58 +0300

    второй commit с исправленными замечаниями

--- precode_test.go
+++ precode_test.go
@@ -19,25 +19,32 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-
-	if status := responseRecorder.Code; status != http.StatusOK {
+	//заменить все на require.Equal
+	//было
+	/*if status := responseRecorder.Code; status != http.StatusOK {
 		t.Errorf("expected status code: %d, got %d", http.StatusOK, status)
-	}
+	}*/
+	//стало
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
 }
 
 // 2) Город, который передаётся в параметре city, не поддерживается.
 // Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
-func TestMainHandlerWhenUnsupportedCity(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=3&city=tula", nil)
+func TestMainHandlerWhenNotUnsupportedCity(t *testing.T) {
+	//город который точно не встретиться
+	// было
+	//req, err := http.NewRequest("GET", "/cafe?count=3&city=tula", nil)
+	// стало
+	req, err := http.NewRequest("GET", "/cafe?count=3&city=UnExistCity", nil)
 	require.NoError(t, err, "Ошибка при создании запроса!")
 
 	responseRecorder := httptest.NewRecorder()
 
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Код ответа 400")
-	require.Contains(t, responseRecorder.Body.String(), "wrong city value", "Тело ответа содержать ошибку wrong city value!")
+	//
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Код ответа не 400")
+	require.Contains(t, responseRecorder.Body.String(), "wrong city value", "Тело ответа содержит ошибку wrong city value!")
 }
 
 // 3) Если в параметре count указано больше, чем есть всего,
